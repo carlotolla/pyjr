8cf84c655f0e845655cd841d1955b2385a3e3a26
SuperPython Jogos <labasence@gmail.com>
2024-11-13 15:11:33 -0300

    Pynoplia-rec/continua.py @2024-11-13 15:11:33

--- rec/continua.py
+++ rec/continua.py
@@ -1,14 +1,14 @@
 """Módulo rec.continua"""
 from vitollino import Jogo, Cena, Elemento, Texto
 Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
-cena1 = Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/imagem_garagem.jpeg?disp=inline&size=G")
+cena1 = Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/foto_escola1.jpeg?disp=inline&size=G")
 cena2 = Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/imagem_patio1.jpeg?disp=inline&size=G", esquerda=cena1)
-cena3 = Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/foto_escola1.jpeg?disp=inline&size=G", direita=cena1, esquerda=cena2).vai()
+cena3 = Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/imagem_garagem.jpeg?disp=inline&size=G", direita=cena1, esquerda=cena2).vai()
 cena2.direita = cena1.esquerda = cena3
 cena1.direita = cena2
-pa = "https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/kisspng-goddess-athena-greek-mythology-deity-greek-goddess-5b218def68fee7.7693559515289256794301-removebg-preview(2).png?disp=inline&size=G"
-achou = "parabens voce achou algo reciclavel agora entregue para a aluna"
-pa = Elemento (pa, x=10, y=150, texto=achou, cena=cena3)
+ds  ="https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/deusa.png.png?disp=inline&size=G"
+achou = " O jogador vai ter que clicar no cesto apenas o que serve de alimento para o urubu em 3 minutos!!! "
+pa = Elemento (ds, x=480, y=250, h=180, texto=achou, cena=cena3)
 alu = "https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/aluno1.png?disp=inline&size=G"
-alu = Elemento (alu, x=250, y=200, h=180, cena=cena2)
+alu = Elemento (alu, x=250, y=200, h=0, cena=cena2)
 texto = Texto(cena2, "precisamos de algo reciclavel para termirnarmos nosso trabalho! procure pela escola").vai()
