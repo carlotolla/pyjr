55536e471a8ec42323aff29f705d45f409628b9b
SuperPython Jogos <labasence@gmail.com>
2024-11-13 16:07:48 -0300

    Pynoplia-pet/main.py @2024-11-13 16:07:48

--- pet/main.py
+++ pet/main.py
@@ -14,8 +14,8 @@ class UmaGarrafa:
              x=100, y=300, cena=p, o=0.5, vai=self.acha_garrafa)
         self.garrafa.o = 0.3 #deixa a garrafa transparente
         self.carta = Elemento(img="/_ativo/agentes/carta_velha.jpg", w=250, h=400,
-             x=ESCONDE, y=50, cena=p, texto="Mas o que está escrito aqui?", foi=self.aparece_kayke)
-        self.kayke = Elemento(img="/_ativo/agentes/kayke.png", w=250, h=400,
+             x=ESCONDE, y=50, cena=p, texto="Mas o que está escrito aqui?", foi=self.aparece_aghata)
+        self.aghata = Elemento(img="/_ativo/agentes/aghata.png", w=250, h=400,
              x=ESCONDE, y=50, cena=p, texto="Deixa que eu vou decifrar")
 
     def acha_garrafa(self, *_):
@@ -25,9 +25,9 @@ class UmaGarrafa:
     def nada_faz(self, *_):
         pass
 
-    def aparece_kayke(self, *_):
+    def aparece_aghata(self, *_):
         # escreva aqui algo semelhante a acha_garrafa
-        self.kayke.x = 100
+        self.aghata.x = 100
         pass
 
 UmaGarrafa()
