a9a19840b684aa7a0de583f256efe91b0e8bb6da
SuperPython Jogos <labasence@gmail.com>
2024-11-14 15:45:46 -0300

    Pynoplia-des/main.py @2024-11-14 15:45:45

--- des/main.py
+++ des/main.py
@@ -1,6 +1,8 @@
 """Módulo des.main"""
-from vitollino import Cena, Jogo, Texto, Elemento
+from vitollino import Cena, Texto, Jogo, Elemento
+from vitollino import Cena, Jogo, Elemento
 Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
 a_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/laboratorio_bancada.jpg?disp=inline&size=G").vai()
-NPC = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/ponto_de-removebg-preview.png?disp=inline&size=G", x=10, y=180, h=200, cena=a_cena)
+npc = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/ponto_de-removebg-preview.png?disp=inline&size=G", x=10, y=180, h=200, cena=a_cena)
 det = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/detetive_mulher.png?disp=inline&size=G", x=350, y=200, h=280, cena=a_cena)
+Texto(a_cena, "Monte o experimento, para um casal de peixes utilizando a agua já tratada. ").vai()
