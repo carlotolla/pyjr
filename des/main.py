0dddbbfe3651993077f01d68c4b72875b29ea899
SuperPython Jogos <labasence@gmail.com>
2024-11-14 16:07:14 -0300

    Pynoplia-des/main.py @2024-11-14 16:07:13

--- des/main.py
+++ des/main.py
@@ -7,6 +7,7 @@ b_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/pizo_branco.
 npc = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/ponto_de-removebg-preview.png?disp=inline&size=G", x=10, y=180, h=200, cena=a_cena)
 det = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/detetive_mulher.png?disp=inline&size=G", x=350, y=200, h=280, cena=a_cena)
 Texto(a_cena, "Monte o experimento, para um casal de peixes utilizando a agua já tratada. ").vai()
-mesa = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/Mesa-em-inox-removebg-preview.png?disp=inline&size=G")
+mesa = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/Mesa-em-inox-removebg-preview.png?disp=inline&size=G", x=180, y=130, h=400, w=350, cena=b_cena)
+
 a_cena.direita = b_cena.esquerda
 a_cena.direita = b_cena
