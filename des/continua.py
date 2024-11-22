9dec594935afc1f2fc695a5b4bea7c78c283fa1d
SuperPython Jogos <labasence@gmail.com>
2024-11-22 16:29:20 -0300

    Pynoplia-des/continua.py @2024-11-22 16:29:20

--- des/continua.py
+++ des/continua.py
@@ -2,5 +2,7 @@
 from vitollino import Cena, Texto, Jogo, Elemento
 from vitollino import Cena, Jogo, Elemento
 Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
-a_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmacia.jpg?disp=inline&size=G")
-b_cena=Cena("")
+b_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmacia.jpg?disp=inline&size=G")
+a_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmaciaex.jpg?disp=inline&size=G").vai()
+b_cena.esquerda = b_cena.direita
+a_cena.direita = a_cena.esquerda
