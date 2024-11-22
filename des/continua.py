f28572c118ba7dc170c0bdb8de630c33ca5f5c36
SuperPython Jogos <labasence@gmail.com>
2024-11-22 17:56:55 -0300

    Pynoplia-des/continua.py @2024-11-22 17:56:55

--- des/continua.py
+++ des/continua.py
@@ -4,6 +4,7 @@ from vitollino import Cena, Jogo, Elemento
 Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
 a_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmacia.jpg?disp=inline&size=G")
 b_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmaciaex.jpg?disp=inline&size=G").vai()
+lixo = Elemento ("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/saco_lixo-removebg-preview.png?disp=inline&size=G", x=30, y=350, h=80, w=65, cena=b_cena)
 b_cena.direita = a_cena.esquerda
 b_cena.direita = a_cena
 a_cena.esquerda = b_cena
