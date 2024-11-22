dce9b20743b7cd6e4890410d93f51fc8f88cfc39
SuperPython Jogos <labasence@gmail.com>
2024-11-22 18:13:24 -0300

    Pynoplia-des/continua.py @2024-11-22 18:13:24

--- des/continua.py
+++ des/continua.py
@@ -6,6 +6,8 @@ a_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmacia.jpg
 b_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmaciaex.jpg?disp=inline&size=G").vai()
 lixo = Elemento ("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/saco_lixo-removebg-preview.png?disp=inline&size=G", x=30, y=350, h=80, w=65, cena=b_cena)
 lixeiras = Elemento ("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/lixeiras-removebg-preview.png?disp=inline&size=G", x=310, y=170, h=350, w=170, cena=b_cena)
+det = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/detetive_mulher.png?disp=inline&size=G", x=390, y=170, h=280, cena=a_cena)
+ddm = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/ddm-removebg-preview.png?disp=inline&size=G", x=94, y=270, h=180, w=160, cena=a_cena)
 b_cena.direita = a_cena.esquerda
 b_cena.direita = a_cena
 a_cena.esquerda = b_cena
