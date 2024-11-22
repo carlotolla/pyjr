45a183dda7bad8f5060ec99d840e0fc94ae58fda
SuperPython Jogos <labasence@gmail.com>
2024-11-22 14:31:25 -0300

    Pynoplia-des/continua.py @2024-11-22 14:31:22

---
+++ des/continua.py
@@ -0,0 +1,6 @@
+"""Módulo des.continua"""
+from vitollino import Cena, Texto, Jogo, Elemento
+from vitollino import Cena, Jogo, Elemento
+Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
+a_cena=Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/farmacia.jpg?disp=inline&size=G")
+b_cena=Cena("")
