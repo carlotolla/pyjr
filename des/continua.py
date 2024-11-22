409d99d39b4ade25b60bc7679e188fe7e69973d8
SuperPython Jogos <labasence@gmail.com>
2024-11-22 19:06:32 -0300

    Pynoplia-des/continua.py @2024-11-22 19:06:32

--- des/continua.py
+++ des/continua.py
@@ -8,6 +8,13 @@ lixo = Elemento ("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/saco_li
 lixeiras = Elemento ("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/lixeiras-removebg-preview.png?disp=inline&size=G", x=310, y=170, h=350, w=170, cena=b_cena)
 det = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/detetive_mulher.png?disp=inline&size=G", x=390, y=170, h=280, cena=a_cena)
 ddm = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/ddm-removebg-preview.png?disp=inline&size=G", x=94, y=270, h=180, w=160, cena=a_cena)
+remedio = "https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/remedio-removebg-preview.png?disp=inline&size=G"
+talco = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/talco-removebg-preview.png?disp=inline&size=G", x=250, y=70, h=140, w=160, cena=a_cena)
+caixadl = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/caixa_de_luva-removebg-preview.png?disp=inline&size=G", x=390, y=70, h=140, w=160, cena=a_cena)
+aparel = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/caixa_ortondontica-removebg-preview.png?disp=inline&size=G", x=10, y=70, h=140, w=160, cena=a_cena)
+
+achou = "parabens voce achou o medicamento correto"
+remedio = Elemento (remedio, x=160, y=80, texto=achou, cena=a_cena)
 b_cena.direita = a_cena.esquerda
 b_cena.direita = a_cena
 a_cena.esquerda = b_cena
