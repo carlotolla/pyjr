d9da709633a49af1e1ba5e69f0a2c81b231d5779
SuperPython Jogos <labasence@gmail.com>
2024-12-11 15:19:08 -0300

    Pynoplia-des/main.py @2024-12-11 15:19:27

--- des/main.py
+++ des/main.py
@@ -1,33 +1,13 @@
 """Módulo des.main"""
 from vitollino import Cena, Texto, Jogo, Elemento
-from cenario import Planilha, Paisagem
-ESCONDE = -4000
-Jogo(style=dict(height="500px", width="650px"), did="_jogo_").z()
+from vitollino import Cena, Jogo, Elemento
+Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
 
-class UmaGarrafa:
-    def __init__(self):
-        imagem_da_praia = "_ativo/agentes/praia.jpeg"
-        mapa_praia = Planilha(imagem_da_praia, conta_lado=4.3)
-        p = Paisagem(mapa_praia.j[0]).vai()
-
-        self.garrafa = Elemento(img="/_ativo/agentes/carta_garrafa.png", w=30, h=10,
-             x=100, y=300, cena=p, o=0.5, vai=self.acha_garrafa)
-        self.garrafa.o = 0.3 #deixa a garrafa transparente
-        self.carta = Elemento(img="/_ativo/agentes/carta_velha.jpg", w=250, h=400,
-             x=ESCONDE, y=50, cena=p, texto="Mas o que está escrito aqui?", foi=self.aparece_kayke)
-        self.kayke = Elemento(img="/_ativo/agentes/kayke.png", w=250, h=400,
-             x=ESCONDE, y=50, cena=p, texto="Deixa que eu vou decifrar")
-
-    def acha_garrafa(self, *_):
-        self.carta.x = 100 # tras a carta para a cena
-        self.garrafa.vai = self.nada_faz
-
-    def nada_faz(self, *_):
-        pass
-
-    def aparece_kayke(self, *_):
-        # escreva aqui algo semelhante a acha_garrafa
-        self.kayke.x = 100
-        pass
-
-UmaGarrafa()
+carfala = "A detetive deve coletar a Água onde houve a morte dos peixes"
+a_cena = Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/Amazonia.jpeg?disp=inline&size=G").vai()
+det = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/detetive_sem_fundo.png?disp=inline&size=G", x=30, y=350, cena=a_cena)
+cur = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/pngtree-brazilian-folklore-festival-mythical-character-red-hair-grass-skirt-kurupira-png-image_3720896-removebg-preview.png?disp=inline&size=G",  x=300, y=230, cena=a_cena)
+pote = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/pote_de_vidro-removebg-preview.png?disp=inline&size=G",x=100, y=300,h=70, cena=a_cena)
+pote = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/galho-_kkkkkkkkkkkkkkkkkk-removebg-preview.png?disp=inline&size=G",x=100, y=250,h=60, cena=a_cena)
+pote = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/pngtree-strawberry-image-on-transparent-background-png-image_4562371-1727424430-removebg-preview.png?disp=inline&size=G",x=500, y=40,h=50, cena=a_cena)
+car = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/12362407-adesivo-de-uma-carta-aberta-de-desenho-animado-vetor-346798243-removebg-preview.png?disp=inline&size=G", texto=carfala,x=350,y=150,h=30,cena=a_cena)
