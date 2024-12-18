f085e6fab3aa7c5bf97d4f0ae477806a537750ae
SuperPython Jogos <labasence@gmail.com>
2024-12-18 11:18:50 -0300

    Pynoplia-des/aventura.py @2024-12-18 11:18:50

--- des/aventura.py
+++ des/aventura.py
@@ -1,7 +1,7 @@
 """Módulo des.main"""
 from vitollino import Cena, Texto, Jogo, Elemento
 from vitollino import Cena, Jogo, Elemento
-Jogo(style=dict(height="450px", width="600px"), did="jogo").z()
+Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
 a_cena = Cena("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/produto_9-2967225546.jpg?disp=inline&size=G").vai()
 saci = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/Saci-1081199002-removebg-preview.png?disp=inline&size=G", x=100, y=350, cena=a_cena)
 homem = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/homem-de-negocios-com-ponto-de-interrogacao-na-cabeca-isolada-no-fundo-branco-86631204-1159827819-removebg-preview.png?disp=inline&size=G",  x=30, y=350, cena=a_cena)
