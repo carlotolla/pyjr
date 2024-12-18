78d94af76b1df5d51308dc6cc37f67a3c3d5b19c
SuperPython Jogos <labasence@gmail.com>
2024-12-18 13:21:15 -0300

    Pynoplia-des/main.py @2024-12-18 13:21:15

--- des/main.py
+++ des/main.py
@@ -1,6 +1,8 @@
 """Módulo des.main"""
 from vitollino import Cena, Texto, Jogo, Elemento
 from vitollino import Cena, Jogo, Elemento
+def nova():
+    import des.aventura
 Jogo(style=dict(height="450px", width="600px"), did="_jogo_").z()
 
 carfala = "A detetive deve coletar a Água onde houve a morte dos peixes"
@@ -10,4 +12,4 @@ cur = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/pngtree-b
 pote = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/pote_de_vidro-removebg-preview.png?disp=inline&size=G",x=100, y=300,h=70, cena=a_cena)
 pote = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/galho-_kkkkkkkkkkkkkkkkkk-removebg-preview.png?disp=inline&size=G",x=100, y=250,h=60, cena=a_cena)
 pote = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/pngtree-strawberry-image-on-transparent-background-png-image_4562371-1727424430-removebg-preview.png?disp=inline&size=G",x=500, y=40,h=50, cena=a_cena)
-car = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/12362407-adesivo-de-uma-carta-aberta-de-desenho-animado-vetor-346798243-removebg-preview.png?disp=inline&size=G", texto=carfala,x=350,y=150,h=30,cena=a_cena)
+car = Elemento("https://activufrj.nce.ufrj.br/studio/Superpython_Jogos/12362407-adesivo-de-uma-carta-aberta-de-desenho-animado-vetor-346798243-removebg-preview.png?disp=inline&size=G", texto=carfala,x=350,y=150,h=30,cena=a_cena, foi=nova)
